c0a2723d8f6549f3a77502306b0ddd4495c44f1b
Mhd-Ishaq <mdishaq3333@gmail.com>
2022-08-30 13:51:42 +0530

    Added  router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,25 @@
-import React from 'react'
-import data from './data'
+import React from "react";
+import { BrowserRouter,Routes,Route,Link } from "react-router-dom";
+import ProductScreen from "./ProductScreen";
+import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
-    <div >
-      <header>
-        <a href="/">Amazon</a>
-      </header>
-      <main>
-        <h1>Featured Products</h1>
-        <div className="products">
-        {
-          data.products.map((product)=>{
-            return(
-            <div className='product' key={product.name}>
-              <a href={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name}/>
-              </a>
-              <div className="product-info">
-              <a href={`/product/${product.slug}`}>
-              <p>{product.name}</p>
-              </a>
-              <p><strong>${product.price}</strong></p>
-              <button>Add to Cart</button>
-              </div>
-            </div>
-            )
-          })
-        }
-        </div>
-      </main>
-    </div>
-  )
-}
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Amazon</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen/>}/>
+            <Route path="/product/:slug" element={<ProductScreen/>}/>
+          </Routes>
+        
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
