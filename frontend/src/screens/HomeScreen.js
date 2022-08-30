0471b980f7d59b91004df00cb7c6933a0b91d495
Mhd-Ishaq <mdishaq3333@gmail.com>
2022-08-30 14:50:18 +0530

    used useReducer to implement our fetch process

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,37 @@
-import React, { useEffect, useState } from "react";
-// import data from "../data";
+import React, { useEffect, useReducer } from "react";
 import { Link } from "react-router-dom";
 import axios from 'axios';
 
+const reducer = (state,action)=>{
+  switch(action.type){
+    case 'FETCH_REQUEST':
+      return{...state,loading:true};
+    case 'FETCH_SUCCESS':
+      return{...state, loading:false, products:action.payload}
+    case 'FETCH_FAIL':
+      return{...state, loading:false, error:action.payload}
+      default:
+        return state;
+  }
+}
+const initialState = {
+  products:[],
+  loading:true,
+  error:"",
+}
+
 const HomeScreen = () => {
-  const [products , setProducts] = useState([]);
+  const [{loading,error,products},dispatch] = useReducer(reducer,initialState);
+
   useEffect(()=>{
     const fetchData = async ()=>{
-      const result = await axios.get('/api/products');
-      setProducts(result.data);
+      dispatch({type:'FETCH_REQUEST'});
+      try{
+        const result = await axios.get('/api/products');
+        dispatch({type:'FETCH_SUCCESS',payload:result.data});
+      }catch(err){
+        dispatch({type:'FETCH_FAIL',payload:err.message});
+      }
     }
     fetchData();
   },[])
@@ -16,7 +39,12 @@ const HomeScreen = () => {
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {products.map((product) => {
+        {
+          loading ?(<div>Loading ...</div>) 
+          :
+          error ? (<div>{error}</div>)
+          :(
+        products.map((product) => {
           return (
             <div className="product" key={product.name}>
               <Link to={`/product/${product.slug}`}>
@@ -33,7 +61,7 @@ const HomeScreen = () => {
               </div>
             </div>
           );
-        })}
+        }))}
       </div>
     </div>
   );
