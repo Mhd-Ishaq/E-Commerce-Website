911021d3f39d1b7aef68ebe60ebac08a67e08ef8
Mhd-Ishaq <mdishaq3333@gmail.com>
2022-08-30 13:34:56 +0530

    Added Featured Products Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,36 @@
 import React from 'react'
+import data from './data'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div >
+      <header>
+        <a href="/">Amazon</a>
+      </header>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+        {
+          data.products.map((product)=>{
+            return(
+            <div className='product' key={product.name}>
+              <a href={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name}/>
+              </a>
+              <div className="product-info">
+              <a href={`/product/${product.slug}`}>
+              <p>{product.name}</p>
+              </a>
+              <p><strong>${product.price}</strong></p>
+              <button>Add to Cart</button>
+              </div>
+            </div>
+            )
+          })
+        }
+        </div>
+      </main>
+    </div>
   )
 }
 
